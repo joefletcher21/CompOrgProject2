7ef48052ae2677904e3590d7da41fdc14f1ec89f
Joseph Fletcher <jckmfletcher@gmail.com>
2019-11-14 23:31:07 -0500

    Create Project2.s
    Initial Commit

---
+++ Project2.s
@@ -0,0 +1,6 @@
+.data 
+   null_errorMessage:   .asciiz  "Input is empty."
+   length_errorMessage:   .asciiz  "Input is too long."
+   base_errorMessage:   .asciiz   "Invalid base-35 number."
+   user_Input:   .space 1000
+   .text
